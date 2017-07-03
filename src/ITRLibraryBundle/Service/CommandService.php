b28ef8d9f8101f6e0298774a1241181cceba751b
Omnicrash <yve@omnicrash.net>
2017-07-03 22:21:40 +0200

    Implemented processing of commands & instruction generation

--- src/ITRLibraryBundle/Service/CommandService.php
+++ src/ITRLibraryBundle/Service/CommandService.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace ITRLibraryBundle\Service;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
 
 /**
  * Processes and executes command-line commands.
@@ -15,35 +16,82 @@ class CommandService
      * CommandService constructor.
      *
      * @param $settings
+     *
+     * @return CommandResponse
      */
     public function __construct($settings)
     {
         $this->$settings = $settings;
     }
 
+    public function processCommand(string $command, string $line = '', $token = false)
+    {
+        $command = strtolower($command);
+
+        // If a token is provided, verify it
+        if ($token !== false && $token !== $this->settings[$command][$token]) {
+            throw new AuthenticationException("Authentication failed");
+        }
+
+        $args = $this->splitArgs($line);
+
+        // Show help if requested, or used without parameters
+        if (($this->settings[$command]['requires_args'] && count($args) === 0)
+            || strtolower($args[0]) === 'help') {
+            return new CommandResponse(
+                $this->getInstructions($command),
+                false
+            );
+        }
+
+        // Execute the command
+        $function = $command.'Command';
+        if (!method_exists($this, $function)) {
+            throw new \BadFunctionCallException('Method '.$command.' not found');
+        }
+
+        return $this->$function($args);
+    }
+
+
+    private function getInstructions($command)
+    {
+        $settings = $this->settings[$command];
+
+        $instructions = 'Usage: /'.$command.' '.$settings['syntax'];
+        if (!empty($settings['instructions'])) {
+            $instructions .="\n".$settings['example'];
+        }
+        if (!empty($settings['example'])) {
+            $instructions .="\n\n/Example: ".$settings['example'];
+        }
+
+        return $instructions;
+    }
+
     /**
      * Split & trim arguments in a command by spaces or possibly quotes.
-     * @param string $command
+     * @param string $line
      *
      * @return array
      */
-    private function splitArgs(string $command)
+    private function splitArgs(string $line)
     {
         $args = [];
-        $lastPos = strlen($command) - 1;
+        $lastPos = strlen($line) - 1;
         $prevPos = -1;
         $char = ' ';
         $quotes = false;
         for ($pos = 0; $pos <= $lastPos; $pos++) {
             // Search for quotes instead of spaces when an opening quote is detected
-            if (!$quotes && ($command[$pos] === '"' || $command[$pos] === "'")) {
-                $char = $command[$pos];
+            if (!$quotes && ($line[$pos] === '"' || $line[$pos] === "'")) {
+                $char = $line[$pos];
                 $quotes = true;
                 $prevPos = $pos;
-            } else if ($command[$pos] === $char || $pos === $lastPos) {
+            } else if ($line[$pos] === $char || $pos === $lastPos) {
                 // Allow adding of empty argument if encased with quotes
                 if ($quotes || $pos > $prevPos + 1) {
-                    $fragment = substr($command, $prevPos + 1, $pos - $prevPos);
+                    $fragment = substr($line, $prevPos + 1, $pos - $prevPos);
                     $args[] = $fragment;
                 }
                 if ($quotes) {
